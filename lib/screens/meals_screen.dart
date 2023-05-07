13a272f343fc954a6d0ed7139f6c7a00d32e98c3
raaz <raaz4995azh@gmail.com>
2023-05-08 00:55:39 +0530

    vpto video 160

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meals_model.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.meals, required this.title});
@@ -10,11 +11,8 @@ class MealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
-      itemCount: meals.length,
-      itemBuilder: (context, index) => Text(
-        meals[index].title,
-      ),
-    );
+        itemCount: meals.length,
+        itemBuilder: (context, index) => MealItem(meal: meals[index]));
     if (meals.isEmpty) {
       content = Center(
         child: Column(mainAxisSize: MainAxisSize.min, children: [
---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meals_model.dart';
+import 'package:meals_app/widgets/meal_item_trait.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key, required this.meal});
+  final Meal meal;
+
+  String get complexityText {
+    return meal.complexity.name.toUpperCase();
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name.toUpperCase();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.hardEdge, //clip any thing outside the body of card
+      margin: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: InkWell(
+        onTap: () => null,
+        child: Stack(children: [
+          FadeInImage(
+            placeholder: MemoryImage(kTransparentImage),
+            image: NetworkImage(meal.imageUrl),
+            fit: BoxFit.cover,
+            height: 200,
+            width: double.infinity, //use as much a possible
+          ),
+          Positioned(
+            //to allow the position parameters
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              //gradient color decoration
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Colors.blue.withOpacity(0.75),
+                      Colors.teal,
+                    ],
+                  )),
+              child: Column(
+                children: [
+                  Text(
+                    meal.title,
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 10, bottom: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        MealItemTrait(
+                            icon: Icons.schedule,
+                            label: '${meal.duration} MIN '),
+                        const SizedBox(width: 12),
+                        MealItemTrait(icon: Icons.work, label: complexityText),
+                        const SizedBox(width: 12),
+                        MealItemTrait(
+                            icon: Icons.attach_money_rounded,
+                            label: affordabilityText),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
